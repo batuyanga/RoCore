c1af5228a953f6419d026d5066b6ac2497cf4926
Myh <mihai.dumitru@gxgnet.ro>
2010-07-20 02:13:26 +0300

    [1] Minor cleanup.

--- src/server/game/Chat/Commands/Level0.cpp
+++ src/server/game/Chat/Commands/Level0.cpp
@@ -105,7 +105,7 @@ bool ChatHandler::HandleServerInfoCommand(const char* /*args*/)
 
 //    PSendSysMessage(_FULLVERSION);
     PSendSysMessage("Created For Romanian Best Private Server: www.wow-romania.ro");
-    PSendSysMessage("Core Revision:" REVISION_NR);
+    PSendSysMessage("Core Revision: " REVISION_NR);
     PSendSysMessage(LANG_CONNECTED_PLAYERS, PlayersNum, MaxPlayersNum);
     PSendSysMessage(LANG_CONNECTED_USERS, activeClientsNum, maxActiveClientsNum, queuedClientsNum, maxQueuedClientsNum);
     PSendSysMessage(LANG_UPTIME, uptime.c_str());
--- src/server/worldserver/Master.cpp
+++ src/server/worldserver/Master.cpp
@@ -203,7 +203,7 @@ int Master::Run()
     BigNumber seed1;
     seed1.SetRand(16 * 8);
 
-    sLog.outString( "%s (core-daemon)", "TrinityCore Custom Revision:" REVISION_NR);
+    sLog.outString( "%s (core-daemon)", "TrinityCore Custom Revision: " REVISION_NR);
     sLog.outString( "<Ctrl-C> to stop.\n" );
 
     sLog.outString( " ______                       __");
