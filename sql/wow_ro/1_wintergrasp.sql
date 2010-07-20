1f61e4b13cfc096d9bdb2cff1971c3b038471e61
Myh <mihai.dumitru@gxgnet.ro>
2010-07-20 13:40:04 +0300

    [17] More sql files moved to reflect much better the new revisions and structure.

--- sql/wow_ro/1_wintergrasp.sql
+++ sql/wow_ro/1_wintergrasp.sql
@@ -1,4 +1,21 @@
+DELETE FROM `command` WHERE name IN ('wg','wg enable','wg start','wg status','wg stop','wg switch','wg timer');
+INSERT INTO `command` VALUES ('wg', '3', 'Syntax: .wg $subcommand.');
+INSERT INTO `command` VALUES ('wg enable', '3', 'Syntax: .wg enable [on/off] Enable/Disable Wintergrasp outdoorPvP.');
+INSERT INTO `command` VALUES ('wg start', '3', 'Syntax: .wg start\r\nForce Wintergrasp battle start.');
+INSERT INTO `command` VALUES ('wg status', '3', 'Syntax: .wg status\r\nWintergrasp info, defender, timer, wartime.');
+INSERT INTO `command` VALUES ('wg stop', '3', 'Syntax: .wg stop\r\nForce Wintergrasp battle stop (No rewards).');
+INSERT INTO `command` VALUES ('wg switch', '3', 'Syntax: .wg switch\r\nSwitchs Wintergrasp defender team.');
+INSERT INTO `command` VALUES ('wg timer', '3', 'Syntax: .wg timer $minutes\r\nChange the current timer. Min value = 1, Max value 60 (Wartime), 1440 (Not Wartime)');
+/* Temp removed gameobject stopping you getting to the relic
+* 60070 - [Wintergrasp Keep Collision Wall X:5396.209961 Y:2840.010010 Z:432.268005 MapId:571
+* 60476 - [Doodad_WG_Keep_Door01_collision01 X:5397.109863 Y:2841.540039 Z:425.901001 MapId:571]*/
+DELETE FROM gameobject WHERE guid = '60070';
+DELETE FROM gameobject WHERE guid = '60476';
+
+/* Change incorrect spells used by Defender's Portal's */
+UPDATE `gameobject_template` SET `data0`=54643 WHERE `entry` in(190763,192819);
 DELETE FROM `trinity_string` WHERE entry IN (756,757,758,759,760,761,762,763,764,765,766,767,768,769,770,771,772,780,781);
+
 INSERT INTO `trinity_string` VALUES ('756', 'Battle begins!', '', '', '', '', '', '', '', '');
 INSERT INTO `trinity_string` VALUES ('757', '%s has successfully defended the fortress!', '', '', '', '', '', '', '', '');
 INSERT INTO `trinity_string` VALUES ('758', '%s has taken over the fortress!', '', '', '', '', '', '', '', '');
@@ -19,4 +36,3 @@ INSERT INTO `trinity_string` VALUES ('772', 'You have reached Rank 2: First Lieu
 INSERT INTO `trinity_string` VALUES ('773', 'Not enough players to begin the battle.', '', '', '', '', '', '', '', '');
 INSERT INTO `trinity_string` VALUES ('780', '30 minutes left before the battle of Wintergrasp!', '', '', '', '', '', '', '', '');
 INSERT INTO `trinity_string` VALUES ('781', '10 minutes left before the battle of Wintergrasp!', '', '', '', '', '', '', '', '');
-
