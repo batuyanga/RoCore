46215373d5da17480dd3d44ec1d1757ab7cd5a0f
Myh <mihai.dumitru@gxgnet.ro>
2010-07-20 20:35:22 +0300

    [29] Correct sql query.

--- sql/wow_ro/27_world_command.sql
+++ sql/wow_ro/27_world_command.sql
@@ -1,3 +1,4 @@
 ALTER TABLE db_version CHANGE COLUMN required_3_world_icecrown_citadel required_27_world_command bit;
 
+DELETE FROM `command` WHERE `name` IN ('achievementadd');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES ('achievementadd', '5', 'Syntax: .achievementadd #id');
