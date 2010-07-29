de4523f227984a8a010067029ea30b9fa0a3f045
Myh <mihai.dumitru@gxgnet.ro>
2010-07-29 10:38:50 +0300

    [181] Other WG updates.

--- src/server/game/Chat/Chat.h
+++ src/server/game/Chat/Chat.h
@@ -500,6 +500,14 @@ class ChatHandler
         bool HandleWintergraspSwitchTeamCommand(const char *args);
         bool HandleWintergraspTimerCommand(const char *args);
 
+        bool HandleWintergraspStatusCommand(const char *args);
+        bool HandleWintergraspStartCommand(const char *args);
+        bool HandleWintergraspStopCommand(const char *args);
+        bool HandleWintergraspEnableCommand(const char *args);
+        bool HandleWintergraspSwitchTeamCommand(const char *args);
+        bool HandleWintergraspTimerCommand(const char *args);
+
+
         bool HandleHelpCommand(const char* args);
         bool HandleCommandsCommand(const char* args);
         bool HandleStartCommand(const char* args);
